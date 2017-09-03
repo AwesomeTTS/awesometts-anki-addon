f2f0e2177fbe6b77d5e532aa014c363a3f964537
Wojciech Kapcia <wojtek@tigase.org>
2017-09-03 15:52:31 -0300

    disable certificate checking while making https connection based on https://www.python.org/dev/peps/pep-0476/#opting-out

--- awesometts/service/base.py
+++ awesometts/service/base.py
@@ -520,6 +520,17 @@ class Service(object):
         assert method in ['GET', 'POST'], "method must be GET or POST"
         from urllib2 import urlopen, Request, quote
 
+        import ssl
+
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            # Legacy Python that doesn't verify HTTPS certificates by default
+            pass
+        else:
+            # Handle target environment that doesn't support HTTPS verification
+            ssl._create_default_https_context = _create_unverified_https_context
+
         targets = targets if isinstance(targets, list) else [targets]
         targets = [
             (target, None) if isinstance(target, basestring)
