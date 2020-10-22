268207e713fb8e0b33b895a93a524397e3debc56
luc wastiaux <luc.wastiaux@airpost.net>
2020-10-22 22:13:15 +0800

    working playback from homescreen

--- awesometts/gui/homescreen.py
+++ awesometts/gui/homescreen.py
@@ -1,10 +1,37 @@
 import aqt
 import anki.hooks
 
+def failure(exception, text):
+    # don't do anything, can't popup any dialogs
+    print(f"* failure: {exception}")
+
 
 def makeLinkHandler(addon):
     def linkHandler(deckbrowser, url):
+
         print(f"* linkHandler {url}")
+
+        (cmd, arg) = url.split(":")
+
+        if cmd == "awesomettsplayback":
+
+            callbacks = dict(
+                okay=addon.player.preview,
+                fail=failure
+            )
+
+            text = arg
+            awesometts_preset_name = 'Aria Neural'
+            preset = addon.config['presets'][awesometts_preset_name]            
+
+            addon.router(
+                svc_id=preset['service'],
+                text=text,
+                options=preset,
+                callbacks=callbacks
+            )                                
+
+
     
     return linkHandler
 
