42c4e9e1a2f4722c949d3812d22ee397bffa5f91
luc wastiaux <luc.wastiaux@airpost.net>
2020-10-26 21:10:33 +0800

    comment out printfs

--- awesometts/gui/homescreen.py
+++ awesometts/gui/homescreen.py
@@ -4,10 +4,6 @@ import json
 import base64
 import aqt
 
-def failure(exception, text):
-    # don't do anything, can't popup any dialogs
-    print(f"* failure: {exception}")
-
 
 def makeLinkHandler(addon):
     def playbackFailure(exception, text):
@@ -16,19 +12,19 @@ def makeLinkHandler(addon):
 
     def linkHandler(deckbrowser, url):
 
-        print(f"* linkHandler {url}")
+        # print(f"* linkHandler {url}")
 
         if url.startswith('awesomettsplayback:'):
             # load the json part
             colon_index = url.find(':')
 
             base64_str = url[colon_index+1:]
-            print(f"* base64 string: {base64_str}")
+            # print(f"* base64 string: {base64_str}")
 
             # base64_bytes = str.encode(base64_str)
             json_bytes = base64.b64decode(base64_str)
             json_str = json_bytes.decode('utf-8')
-            print(f"* json string: {json_str}")
+            # print(f"* json string: {json_str}")
 
             data = json.loads(json_str)
 
@@ -57,9 +53,6 @@ def makeLinkHandler(addon):
 def makeDeckBrowserRenderContent(addon):
 
     def on_deckbrowser_will_render_content(deck_browser, content):
-        print(f"** on_deckbrowser_will_render_content")
-        #print(deck_browser)    
-        #print(content)
 
         night_mode = aqt.mw.pm.night_mode()
 
