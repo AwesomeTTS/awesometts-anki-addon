f0d22c156f7e1b3ad8deadf3d8af6bd80c6e3c5a
luc wastiaux <luc.wastiaux@airpost.net>
2021-01-30 16:15:13 +0800

    re-purpose naver clova premium as naver clova voice

--- awesometts/service/naverclovapremium.py
+++ awesometts/service/naverclovapremium.py
@@ -26,6 +26,10 @@ import datetime
 import requests
 import urllib
 from .base import Service
+from .languages import Language
+from .languages import Gender
+from .languages import Voice
+from typing import List
 
 __all__ = ['NaverClovaPremium']
 
@@ -33,6 +37,35 @@ VOICE_LIST = [
     ('nara', 'Korean, female voice'),
 ]
 
+
+class NaverVoice(Voice):
+    def __init__(self, language, name, gender, description, voice_type):
+        self.language = language
+        self.name = name
+        self.gender = gender
+        self.description = description
+        self.voice_type = voice_type
+
+    def get_voice_key(self):
+        return {
+            'name': self.name
+        }
+
+    def get_language(self) -> Language:
+        return self.language
+
+    def get_gender(self) -> Gender:
+        return self.gender
+
+    def get_key(self) -> str:
+        return self.name
+
+    def get_voice_shortname(self):
+        return f'{self.description} ({self.voice_type})'
+
+    def get_description(self) -> str:
+        value = f"{self.language.lang_name}, {self.gender.name}, {self.voice_type}, {self.description}"
+        return value
 
 
 class NaverClovaPremium(Service):
@@ -60,24 +93,57 @@ class NaverClovaPremium(Service):
 
         return [dict(key='clientid', label="API Client Id", required=True),
             dict(key='clientsecret', label="API Client Secret", required=True)]
+    
+    def get_voices(self) -> List[NaverVoice]:
+        return [
+            NaverVoice(Language.ko_KR, 'mijin', Gender.Female, 'Mijin', 'General'),
+            NaverVoice(Language.ko_KR, 'jinho', Gender.Male, 'Jinho', 'General'),
+            NaverVoice(Language.en_US, 'clara', Gender.Female, 'Clara', 'General'),
+            NaverVoice(Language.en_US, 'matt', Gender.Male, 'Matt', 'General'),
+            NaverVoice(Language.ja_JP, 'shinji', Gender.Male, 'Shinji', 'General'),
+            NaverVoice(Language.zh_CN, 'meimei', Gender.Female, 'Meimei', 'General'),
+            NaverVoice(Language.zh_CN, 'liangliang', Gender.Male, 'Liangliang', 'General'),
+            NaverVoice(Language.es_ES, 'jose', Gender.Male, 'Jose', 'General'),
+            NaverVoice(Language.es_ES, 'carmen', Gender.Female, 'Carmen', 'General'),
+
+            NaverVoice(Language.ko_KR, 'nara', Gender.Female, 'Nara', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nminsang', Gender.Male, 'Minsang', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nhajun', Gender.Male, 'Hajoon', 'Premium (Child)'),
+            NaverVoice(Language.ko_KR, 'ndain', Gender.Female, 'Dain', 'Premium (Child)'),
+            NaverVoice(Language.ko_KR, 'njiyun', Gender.Female, 'Jiyoon', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nsujin', Gender.Female, 'Sujin', 'Premium'),
+            NaverVoice(Language.ko_KR, 'njinho', Gender.Male, 'Jinho', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nsinu', Gender.Male, 'Shinwoo', 'Premium'),
+            NaverVoice(Language.ko_KR, 'njihun', Gender.Male, 'Jihoon', 'Premium'),
+
+            NaverVoice(Language.ja_JP, 'ntomoko', Gender.Female, 'Tomoko', 'Premium'),
+            NaverVoice(Language.ja_JP, 'nnaomi', Gender.Female, 'Naomi', 'Premium'),
+            NaverVoice(Language.ja_JP, 'nsayuri', Gender.Female, 'Sayuri', 'Premium'),
+
+            NaverVoice(Language.ko_KR, 'ngoeun', Gender.Female, 'Koeun', 'Premium'),
+            NaverVoice(Language.ko_KR, 'neunyoung', Gender.Female, 'Eunyoung', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nsunkyung', Gender.Female, 'Sunkyung', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nyujin', Gender.Female, 'Yujin', 'Premium'),
+            
+            NaverVoice(Language.ko_KR, 'ntaejin', Gender.Male, 'Taejin', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nyoungil', Gender.Male, 'Youngil', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nseungpyo', Gender.Male, 'Seungpyo', 'Premium'),
+            NaverVoice(Language.ko_KR, 'nwontak', Gender.Male, 'Wontak', 'Premium'),                
+        ]
+
+    def get_voice_list(self):
+        return [(voice.get_key(), voice.get_description()) for voice in self.get_voices()]
 
     def options(self):
 
         return [
             dict(key='voice',
                  label="Voice",
-                 values=VOICE_LIST,
+                 values=self.get_voice_list(),
                  transform=lambda value: value),
             dict(
                 key='speed',
                 label="Speed",
-                values=(-5, 5),
-                transform=int,
-                default=0,
-            ),
-            dict(
-                key='volume',
-                label="Volume",
                 values=(-5, 5),
                 transform=int,
                 default=0,
@@ -88,14 +154,7 @@ class NaverClovaPremium(Service):
                 values=(-5, 5),
                 transform=int,
                 default=0,
-            ),
-            dict(
-                key='emotion',
-                label="Emotion",
-                values=(0, 2),
-                transform=int,
-                default=0,
-            )            
+            )
         ]
 
 
@@ -107,12 +166,10 @@ class NaverClovaPremium(Service):
         encText = urllib.parse.quote(text)
         voice = options['voice']
         speed = options['speed']
-        volume = options['volume']
         pitch = options['pitch']
-        emotion = options['emotion']
 
-        data = f"speaker={voice}&speed={speed}&volume={volume}&pitch={pitch}&emotion={emotion}&text={encText}"
-        url = "https://naveropenapi.apigw.ntruss.com/voice-premium/v1/tts"
+        data = f"speaker={voice}&speed={speed}&pitch={pitch}&text={encText}"
+        url = 'https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts'
         self._logger.debug(f"url: {url}, data: {data}")
         request = urllib.request.Request(url)
         request.add_header("X-NCP-APIGW-API-KEY-ID",client_id)
