a4fd5b2d958af8c068ad860653e83b6354e12392
kelciour <kelciour@gmail.com>
2021-03-17 03:04:33 +0300

    enable problematic javascript code

--- awesometts/gui/generator.py
+++ awesometts/gui/generator.py
@@ -764,7 +764,7 @@ class EditorGenerator(ServiceDialog):
 
         for origin in [
                 lambda: from_note(web.selectedText()),
-                # lambda: from_note(exec_javascript()), # seems to go into infinite loop waiting for js callback, disable for now
+                lambda: from_note(exec_javascript()),
                 lambda: try_clipboard('html'),
                 lambda: try_clipboard('text'),
         ]:
